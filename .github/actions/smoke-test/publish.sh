34ed3426e4dfc38993c1f5fa90df857a034d2268
Alexander Ilyin <alexander@ilyin.eu>
2025-01-29 01:06:47 +0000

    PC-323: Use template and build image.

--- .github/actions/smoke-test/publish.sh
+++ .github/actions/smoke-test/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-TEMPLATE_ID="$1"
 
-# TODO: @alexanderilyin run .github/workflows/release.sh
-devcontainer templates apply --template-id "ghcr.io/partcad/devcontainers-templates/${TEMPLATE_ID}:1.3.0" --workspace-folder "build/${TEMPLATE_ID}" --log-level trace
-# TODO: @alexanderilyin use "--label" option to add metadata to the image
-devcontainer build --push --log-level trace --image-name "ghcr.io/partcad/devcontainer-${TEMPLATE_ID}:1.3.0" --workspace-folder "build/${TEMPLATE_ID}"
+for TEMPLATE_ID in $(find src/ -mindepth 1 -maxdepth 1 -type d -print0 | xargs --null -n1 basename); do
+    devcontainer templates apply --template-id "ghcr.io/partcad/devcontainers-templates/${TEMPLATE_ID}:1.3.0" --workspace-folder "build/${TEMPLATE_ID}" --log-level trace
+    # TODO: @alexanderilyin use "--label" option to add metadata to the image
+    devcontainer build --push --log-level trace --image-name "ghcr.io/partcad/devcontainer-${TEMPLATE_ID}:1.3.0" --workspace-folder "build/${TEMPLATE_ID}"
+done
